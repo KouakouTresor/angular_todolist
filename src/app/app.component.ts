a45df1035249a81d614e9f2af34bbfb367a914cb
NguedjuiTresor <tresorkouakou@gmail.com>
2024-08-07 18:27:29 +0200

    add todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,49 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './Todo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todolist';
+
+  private _todos: Todo[] = [
+    { id: 1, content: 'Learn Angular', done: false, createdAt: new Date() },
+    { id: 2, content: 'Eat better', done: true, createdAt: new Date() },
+    {
+      id: 3,
+      content: 'Drink a lot of water',
+      done: false,
+      createdAt: new Date(),
+    },
+    { id: 4, content: 'Sleep more', done: true, createdAt: new Date() },
+  ];
+
+  get todos() {
+    return this._todos;
+  }
+
+  handleClick() {
+    //console.log('Clicked');
+  }
+
+  onSubmit($event: SubmitEvent, arg1: string) {
+    $event.preventDefault();
+    this.todos.push({
+      id: this.todos.length + 1,
+      content: arg1,
+      done: false,
+      createdAt: new Date(),
+    });
+  }
+
+  toggleChange(todo: Todo) {
+    return todo.done = !todo.done
+  }
 }
